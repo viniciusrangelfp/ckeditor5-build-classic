617dbd377a14119e92ba71cad091db4c550e6b77
unknown <vinicius.rangel@ad4pixels.com.br>
2019-06-19 17:29:01 -0300

    AutoSave plugin

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -27,6 +27,7 @@ import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
+import AutoSave from '@ckeditor/ckeditor5-autosave/src/autosave';
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
@@ -52,7 +53,8 @@ ClassicEditor.builtinPlugins = [
 	Paragraph,
 	PasteFromOffice,
 	Table,
-	TableToolbar
+	TableToolbar,
+	AutoSave
 ];
 
 // Editor configuration.
